40356d16da88ffddc595461c346e634fb9a228de
Hinal <patel.hinal@outlook.com>
2024-06-04 21:52:16 +0530

    Values and Variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -2,3 +2,25 @@
 let js = "amazing";
 if (js === "amazing") alert("Javascript is FUN");
 console.log(4 + 28 + 23 - 10);
+
+//Values and Variables
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Hinal";
+
+console.log(firstName);
+console.log(firstName);
+
+//Variable Name convention
+
+let hinal_patel = "HM";
+let $function = 27;
+
+let person = "Hinal";
+let PI = 3.14;
+
+let myFirstJob = "Wen Analyst";
+let myCurrentJob = "Software Developer";
+
+console.log(myCurrentJob);

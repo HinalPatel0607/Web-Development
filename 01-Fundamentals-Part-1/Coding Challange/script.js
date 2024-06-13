41762b9a8e579f04746a32c47d864881daa4779c
Hinal <patel.hinal@outlook.com>
2024-06-13 22:39:38 +0530

    Coding Challenge 3

--- 01-Fundamentals-Part-1/Coding Challange/script.js
+++ 01-Fundamentals-Part-1/Coding Challange/script.js
@@ -1,3 +1,5 @@
+//Coding Challenge 1 and 2
+
 const markWeight = 78;
 const markHeight = 1.69;
 const johnWeight = 92;
@@ -36,4 +38,36 @@ if (bmiMark > bmiJohn) {
   console.log(`Mark's BMI is higher ${bmiMark} than John's BMI ${bmiJohn}.`);
 } else {
   console.log(`John's BMI ${bmiJohn} is higher than Mark's BMI ${bmiMark}.`);
+}
+
+//Coding Challenge 3
+
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas) {
+  console.log("Dolphins won the trophy🏆.");
+} else if (scoreDolphins < scoreKoalas) {
+  console.log("Koalas won the trophy🏆.");
+} else if (scoreDolphins === scoreKoalas) {
+  console.log("There is a tie so both team won the trophy🏆.");
+}
+
+const scoreDolphin = (97 + 112 + 101) / 3;
+const scoreKoala = (109 + 95 + 106) / 3;
+console.log(scoreDolphin, scoreKoala);
+
+if (scoreDolphin > scoreKoala && scoreDolphin >= 100) {
+  console.log("Dolphin won the trophy🏆.");
+} else if (scoreDolphin < scoreKoala && scoreKoala >= 100) {
+  console.log("Koala won the trophy🏆.");
+} else if (
+  scoreDolphin === scoreKoala &&
+  scoreDolphin >= 100 &&
+  scoreKoala >= 100
+) {
+  console.log("There is a tie so both team won the trophy🏆.");
+} else {
+  console.log("No one won the trophy🏆.");
 }

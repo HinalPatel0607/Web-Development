47acde8c803d8a3276191616276e036bb6c6345c
Hinal <patel.hinal@outlook.com>
2024-06-20 21:19:09 +0530

    Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -8,3 +8,24 @@ if (hasDriversLicence) console.log("I can drive.");
 
 //const interface = Audio;
 //const private = 123;
+
+//Functions
+
+function logger() {
+  console.log("My name is Hinal");
+}
+logger();
+logger();
+
+function foodProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+const appleJuice = foodProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrnageJuice = foodProcessor(3, 4);
+console.log(appleOrnageJuice);
+
+const num = Number("23");

a15de4ab1904ab726da7874c1e3e45e22a6bb0e1
Hinal <patel.hinal@outlook.com>
2024-07-03 21:58:04 +0530

    Coding Challenge 7

--- 01-Fundamentals-Part-1/Coding Challange/script.js
+++ 01-Fundamentals-Part-1/Coding Challange/script.js
@@ -123,3 +123,39 @@ const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 console.log(bills);
 console.log(tips);
 console.log(totals);
+
+////Coding Challenge 7
+
+const mark = {
+  fullName: "Mark Miller",
+  weight: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.weight / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  weight: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.weight / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+mark.calcBMI();
+john.calcBMI();
+console.log(mark.bmi, john.bmi);
+
+if (mark.bmi > john.bmi) {
+  console.log(
+    `${mark.fullName}'s BMI ${mark.bmi} is higher than ${john.fullName}'s BMI ${john.bmi}.`
+  );
+} else if (john.bmi > mark.bmi) {
+  console.log(
+    `${john.fullName}'s BMI ${john.bmi} is higher than ${mark.fullName}'s BMI ${mark.bmi}.`
+  );
+}

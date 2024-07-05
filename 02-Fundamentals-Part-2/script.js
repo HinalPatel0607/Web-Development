5c95107a943360c1a4c6fd368805de0c98b93393
Hinal <patel.hinal@outlook.com>
2024-07-06 02:46:22 +0530

    Looping Backwards and Loops in Loops

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -338,3 +338,16 @@ for (let i = 0; i < Hinall.length; i++) {
   if (typeof Hinall[i] === "number") break;
   console.log(Hinall[i], typeof Hinall[i]);
 }
+
+//Looping Backwards and Loops in Loops
+
+for (let i = Hinall.length - 1; i >= 0; i--) {
+  console.log(i, Hinall[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`---Starting exercise ${exercise}`);
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise: ${exercise}: Lifting weight repetition ${rep} 🏋️‍♂️🏋️‍♀️`);
+  }
+}
